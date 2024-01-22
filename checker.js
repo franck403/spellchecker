99b0a578e278ad499b58cf7ea16507266b1f1f8d
franck403 <79372051+franck403@users.noreply.github.com>
2024-01-22 11:14:49 -0500

    Base file

--- checker.js
+++ checker.js
@@ -1,1 +1,35 @@
+// load js file
+function loadJS(url) { 
+    let scriptToAdd = document.createElement('script');     
+    scriptToAdd.type = 'text/javascript'; 
+    scriptToAdd.src = url
+    document.querySelector("head").append(scriptToAdd); 
+    return True
+}
+function loadWorldsList(url) {
+    var request = new XMLHttpRequest();
+    request.open("GET", url, false); // `false` makes the request synchronous
+    request.send(null);
 
+    if (request.status === 200) {
+        console.log(request.responseText);
+        return request.responseText;
+    }
+    return null;
+}
+
+// cdn url to fetch to get the words
+english = 'https://cdn.jsdelivr.net/gh/dwyl/english-words@master/words.txt'
+french = 'https://cdn.jsdelivr.net/gh/Taknok/French-Wordlist@master/francais.txt'
+
+// wagner fischer algorithm
+wagnerURl = "https://cdn.jsdelivr.net/npm/wagner-fischer@1.0.0/lib/wagner-fischer.min.js"
+
+// load js files and load dict
+loadJS(wagnerURL)
+loadWorldsList(french)
+
+// check function
+function check(word) {
+    wagnerFischer('saturday', 'sunday')
+}
