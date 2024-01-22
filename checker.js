32d96e7f8ef208a76a229444d42ebc9f5322b093
franck403 <79372051+franck403@users.noreply.github.com>
2024-01-22 12:09:35 -0500

    testing

--- checker.js
+++ checker.js
@@ -2,11 +2,11 @@
 import wagnerFischer from 'https://cdn.jsdelivr.net/npm/wagner-fischer@1.0.0/+esm'
 
 // load js file
-function loadJS(url) { 
-    let scriptToAdd = document.createElement('script');     
-    scriptToAdd.type = 'text/javascript'; 
+function loadJS(url) {
+    let scriptToAdd = document.createElement('script');
+    scriptToAdd.type = 'text/javascript';
     scriptToAdd.src = url
-    document.querySelector("head").append(scriptToAdd); 
+    document.querySelector("head").append(scriptToAdd);
     return True
 }
 function loadWorldsList(url) {
@@ -29,11 +29,44 @@ var french = 'https://cdn.jsdelivr.net/gh/Taknok/French-Wordlist@master/francais
 var wagner = wagnerFischer
 
 // load js files and load dict
-loadWorldsList(french)
+var wordList = loadWorldsList(french)
+window.wordList = wordList
 
 // check function
-function check(word,possible) {
+function check(word, possible) {
     return wagnerFischer(word, possible)
 }
 
 window.check = check
+function setup() {
+    var re = new RegExp(setup);
+    var words = text => text.toLowerCase().match(re);
+    var wordList = words(window.wordList);
+    wordList.forEach(word => {
+        let count = wordList.get(word) || 0;
+        wordList.set(word, count + 1);
+    
+        var P = (word, N = Array.from(WORDS.values()).reduce((a, b) => a + b)) => WORDS.get(word) / N;
+    })
+    var correction = word => {
+        var candidates = known([word]) || known(edits1(word)) || known(edits2(word)) || [word];
+        return candidates.reduce((a, b) => P(a) > P(b) ? a : b);
+    };
+    
+    var known = words => new Set(words.filter(word => WORDS.has(word)));
+    
+    var edits1 = word => {
+        var letters = 'abcdefghijklmnopqrstuvwxyz';
+        var splits = [(word.slice(0, i), word.slice(i))];
+        var deletes = splits.filter(([L, R]) => R).map(([L, R]) => L + R.slice(1));
+        var transposes = splits.filter(([L, R]) => R.length > 1).map(([L, R]) => L + R[1] + R[0] + R.slice(2));
+        var replaces = splits.filter(([L, R]) => R).flatMap(([L, R]) => [...letters].map(c => L + c + R.slice(1)));
+        var inserts = splits.flatMap(([L, R]) => [...letters].map(c => L + c + R));
+        return new Set([...deletes, ...transposes, ...replaces, ...inserts]);
+    };
+    
+    var edits2 = word => {
+        var edits1Set = new Set(edits1(word));
+        return Array.from(edits1Set).flatMap(e1 => Array.from(edits1Set).map(e2 => e1 + e2));
+    }
+}
